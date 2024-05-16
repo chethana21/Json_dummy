0e8c537f635d14c49aaac569368f649835245c5b
chethana21 <66832766+chethana21@users.noreply.github.com>
2024-05-16 17:08:31 +0530

    Delete Employees directory

--- Employees/main.py
+++
@@ -1,7 +0,0 @@
-from action_handler import ActionHandler
-def main():
-    
-    action_handler = ActionHandler("https://dummy.restapiexample.com/api/v1/employees")
-    action_handler.get_all_users_data()
-if __name__ == "__main__":
-    main()
